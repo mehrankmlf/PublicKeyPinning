56175eaa32cd732cdfb803ba4b1430892836d142
Mehran Kamalifard <mehran.kmlf@icloud.com>
2022-06-23 15:49:23 +0430

    Initial Commit

---
+++ PublicKeyPinning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PublicKeyPinning
+//
+//  Created by Mehran on 4/2/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
